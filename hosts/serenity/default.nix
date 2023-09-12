8de68c72c8b2ffd72e52bdf77e9a59825ddaa1e5
fushiii <fushitheimortal@gmail.com>
2023-09-12 18:14:31 -0300

    feat: new host and user

---
+++ hosts/serenity/default.nix
@@ -0,0 +1,46 @@
+{ lib, inputs, ... }:
+with lib;
+with lib.my;
+{
+  imports = [
+    ../home.nix
+    ./hardware-configuration.nix
+
+    # The users for this host.
+    (getUser "diegofariasm")
+  ];
+  # Automagically format the disk
+  # and mount the partitions.
+  disko.devices = import ./disko.nix { disks = [ "/dev/sda" ]; };
+
+  ## Modules
+  modules = {
+    desktop = {
+      hypr.enable = true;
+      display = {
+        gdm.enable = true;
+      };
+    };
+
+    # These are generally by a lof of packages.
+    # So, instead of i having to wrap to be able to install them,
+    # i just install it globally and use it like that. I can just override it anyway.
+    dev = {
+      cc.enable = true;
+      rust.enable = true;
+    };
+
+    hardware = {
+      audio.enable = true;
+      zram.enable = true;
+      fs.enable = true;
+    };
+
+    services = {
+      ssh.enable = true;
+      docker.enable = true;
+      udiskie.enable = true;
+    };
+  };
+
+}
